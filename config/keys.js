0a0641c40a3285e36c2f3083b48b3df0255f1a8f
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-30 22:28:18 -0400

    creating the JWT

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: 
-    'mongodb://surekha:surekha123@ds117423.mlab.com:17423/merndevconnector'
+    'mongodb://surekha:surekha123@ds117423.mlab.com:17423/merndevconnector',
+    secretOrKey: 'secret'
 };
