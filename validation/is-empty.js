3177df3b262f51a324e39517d36977ea8a9d4040
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-02 16:36:02 -0400

    Validation Handlers-1

---
+++ validation/is-empty.js
@@ -0,0 +1,17 @@
+// function isEmpty(value) {
+//     return (
+//         value === undefined ||
+//         value === null ||
+//         (typeof value === 'object' && Object.keys(value).length === 0) ||
+//         (typeof value === 'string' && value.trim().length === 0)
+//     );
+// }
+
+// Refactoring the above code
+const isEmpty = value => 
+    value === undefined ||
+    value === null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
