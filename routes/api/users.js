e57fe341acf51b41a7a583ef40706b752e134d21
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-02 16:35:43 -0400

    Validation Handlers-1

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+//Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+
 // Load User model
 const User = require('../models/User');
 
@@ -18,6 +21,11 @@ router.get('/test', (req, res) => res.json({msg: "Users works"}));
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+    // Check Validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
     User.findOne({ email: req.body.email })
         .then(user => {
             if(user){
