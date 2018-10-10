e3f9e0faff73af9f3d0857486ed69982ba783a82
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-09 23:14:08 -0400

    Profile Field Validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
+
+
 // Load Profile Model
 const profile = require('../models/Profile');
 
@@ -38,6 +42,14 @@ router.post(
     '/', 
     passport.authenticate('jwt', {session: false}), 
     (req, res) => {
+        const { errors, isValid } = validateProfileInput(req.body);
+
+        // Check validation
+        if(!isValid) {
+            // Return any errors with 400 status
+            return res.status(400).json(errors);
+        }
+
         // Get fields 
         const profileFields = {};
         profileFields.user = req.user.id;
