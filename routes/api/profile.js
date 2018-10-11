f4429b4fed05a47253bf4f53022e122e5b9177ce
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-10 21:40:10 -0400

    Profile Field Validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -25,6 +25,7 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+        .populate('user', ['name', 'avatar'])
         .then(profile => {
             if(!profile) {
                 errors.noprofile = 'There is no profile for this user';
