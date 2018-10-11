ed6f036b516f15e7d2d6c5c18d9d079213c80c47
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-10 21:57:14 -0400

    More Profile API Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -36,6 +36,24 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
         .catch(err => res.status(404).json(err));
 });
 
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle  
+// @access  Public
+
+router.get('/handle/:handle', (req, res) =>{
+    const errors = {};
+    Profile.findOne({ handle: req.params.handle })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
+
 // @route   POST api/profile
 // @desc    Create or Edit user profile  
 // @access  Private
