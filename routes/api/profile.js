f01916c48f211f7ec867493e837bae15b1d210a5
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-18 10:45:18 -0400

    More Profie API Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -36,6 +36,26 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
         .catch(err => res.status(404).json(err));
 });
 
+// @route   GET api/profile/all
+// @desc    Get all profiles   
+// @access  Public
+router.get('/all', (req, res) => {
+    const errors = {};
+
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profiles => {
+            if(!profiles) {
+                errors.noprofile ='There are no profiles';
+                return res.status(404).json(errors);
+            }
+            res.json(profiles);
+        })
+        .catch(err =>
+            res.status(404).json({profile: 'There are no profiles'})
+            );
+})
+
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle  
 // @access  Public
@@ -75,7 +95,7 @@ router.get('/user/:user_id', (req, res) =>{
 
 // @route   POST api/profile
 // @desc    Create or Edit user profile  
-// @access  Private
+// @access  Private 
 router.post(
     '/', 
     passport.authenticate('jwt', {session: false}), 
