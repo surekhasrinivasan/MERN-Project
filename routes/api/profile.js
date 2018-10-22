1d4dda75026eb99e9e7e541eb9347bf9c6be81b9
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-21 20:30:35 -0400

    Delete User and Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -269,4 +269,19 @@ passport.authenticate('jwt', { session: false }),
         .catch(err => res.status(404).json(err));
 });
 
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', 
+passport.authenticate('jwt', { session: false }), 
+(req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+            User.findOneAndRemove({ _id: req.user.id }).then(() =>
+                res.json({ success: true })
+            );
+        });
+    }
+);
+
+
 module.exports = router;
