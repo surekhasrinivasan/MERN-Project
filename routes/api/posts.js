97eca79cef516a9258b284b64d0c7c3d289d27a8
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-26 16:57:06 -0400

    Post create route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -18,10 +18,17 @@ router.get('/test', (req, res) => res.json({msg: 'Posts works'}));
 // @desc    Create post
 // @access  Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if(!isValid) {
+        // If any errors, send 400 with errors object
+        return res.status(400).json(errors); 
+    }    
     const newPost = new Post({
         text: req.body.text,
         name: req.body.name,
-        avatar: req.body.name,
+        avatar: req.body.avatar,
         user: req.user.id
     });
     
