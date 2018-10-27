a38a1aefb8cc13e2810142d48bd1d9d57e8e1b0c
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-27 07:55:20 -0400

    Get single post by its id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -21,7 +21,18 @@ router.get('/', (req, res) => {
     Post.find()
         .sort({ date: -1 })
         .then(posts => res.json(posts))
-        .catch(err => res.status(404));
+        .catch(err => res.status(404).json({ nopostsfound: 'No posts found'}));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => 
+            res.status(404).json({ nopostfound: 'No post found with that ID' })
+        );
 });
 
 // @route   POST api/posts
