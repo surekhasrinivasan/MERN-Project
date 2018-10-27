f0530512c4dec1de37c7217c266045c2ab1561f5
surekhasrinivasan <surekhasrao@gmail.com>
2018-10-27 07:41:29 -0400

    Get all the posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,6 +14,16 @@ const validatePostInput = require('../../validation/post')
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: 'Posts works'}));
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', (req, res) => {
+    Post.find()
+        .sort({ date: -1 })
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404));
+});
+
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
