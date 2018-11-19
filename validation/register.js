4c3f5b0ebd103527cd899e4abd39790e4da021c6
surekhasrinivasan <surekhasrao@gmail.com>
2018-11-19 17:40:38 -0500

    register.js modified

--- validation/register.js
+++ validation/register.js
@@ -38,7 +38,7 @@ module.exports = function validateRegisterInput(data) {
     }
 
     if(!Validator.equals(data.password, data.password2)) {
-        errors.password2 = 'Passwords must match field';
+        errors.password2 = 'Passwords must match';
     }
 
     return {
