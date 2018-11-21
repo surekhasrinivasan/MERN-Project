40075d44e0b3e923443064ba8f00e3db3d33169a
surekhasrinivasan <surekhasrao@gmail.com>
2018-11-20 21:51:53 -0500

    Redux Store Chrome Extension Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import store from './store';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -10,8 +10,6 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 import './App.css';
-
-const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
