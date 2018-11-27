5d8df61748c3ea983c0e4041e3fb686f8f022060
surekhasrinivasan <surekhasrao@gmail.com>
2018-11-27 16:17:13 -0500

    Redux Login Action Set Current User

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -13,3 +13,20 @@ export const registerUser =(userData, history) => dispatch => {
             })      
         );
 };
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+    axios.post('./api/users/login', userData)
+        .then(res => { 
+            // Save to localStorage
+            const { token } = res.data;
+            // Sect token to localStorage
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth header
+            setAuthToken(token);
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+        }));
+};
